0aee4f9fc5765c1e323ff856f49dd5cb0535fa70
esahcorp <lostkite@outlook.com>
2020-10-02 23:19:54 +0800

    feat(gateway): enable gateway to route request from front end
      1) enable cors
      2) add route rule of product service and admin service

---
+++ 2cmall-gateway/src/main/java/com/cambrian/mall/gateway/config/GatewayCorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.cambrian.mall.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kuma 2020-10-02
+ */
+@Configuration
+public class GatewayCorsConfiguration {
+
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        config.addAllowedOrigin("*");
+//        config.setAllowCredentials(true);
+//        config.setMaxAge(3600L);
+//        corsConfigurationSource.registerCorsConfiguration("/**", config);
+//        return new CorsWebFilter(corsConfigurationSource);
+//    }
+}
--- renren-fast/src/main/java/io/renren/config/CorsConfig.java
+++ renren-fast/src/main/java/io/renren/config/CorsConfig.java
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
+ * <p>
  * https://www.renren.io
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
@@ -17,10 +17,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowCredentials(true)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .maxAge(3600);
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .maxAge(3600);
     }
 }

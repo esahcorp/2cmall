38fdca3b9c6472213f90071cd829f47f0700f1a1
esahcorp <lostkite@outlook.com>
2020-11-12 18:32:14 +0800

    feat(ware): 实现仓库信息查询接口

--- 2cmall-ware/src/main/java/com/cambrian/mall/ware/service/impl/WareInfoServiceImpl.java
+++ 2cmall-ware/src/main/java/com/cambrian/mall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,17 @@
 package com.cambrian.mall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cambrian.common.utils.PageUtils;
 import com.cambrian.common.utils.Query;
-
 import com.cambrian.mall.ware.dao.WareInfoDao;
 import com.cambrian.mall.ware.entity.WareInfoEntity;
 import com.cambrian.mall.ware.service.WareInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,18 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            wrapper.and(wp -> wp.eq("id", key)
+                    .or().like("name", key)
+                    .or().like("address", key)
+                    .or().like("areacode", key));
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
